73fd3925c1fe857f0c8a2b84f4b7abf886db4852
Hendra Ng <hendrang@outlook.com>
2021-03-14 14:05:44 -0600

    initial commit backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '18a536ae33ce7ec55bb9e4958dd333e8'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7fd85c17-7a21-47bb-ad8a-6213acb57f0f'
+};
